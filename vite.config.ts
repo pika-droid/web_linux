b419a4e1c1e7b1423fa0ef6ab4b0bcbae5674325
Ashmit Mandal <ashmitmandal2k04@gmail.com>
2026-06-20 00:36:46 +0530

    chore(01-01): install dependencies and configure vitest scripts
    - Install vitest, jsdom, and @testing-library/react
    
    - Add test and test:run scripts in app/package.json
    
    - Configure test parameters in app/vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'plugin-inspect-react-code'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [inspectAttr(), react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
+  },
+});
